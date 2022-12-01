04d29acd8f93d073580cdec3097ffac830cecf9f
roy <kimathiwaweru@gmail.com>
2022-12-01 11:33:08 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// Code your solutions in this file
+const gifts=["Guadalupe", "Ollie", "Aki"];
+function writeCards(){
+    const response=[];
+    let q;
+    for(q=0; q<gifts.length; q++){
+       response[q]=`Thank you, ${gifts[q]}, for the wonderful surprise gift!`
+      
+    }
+    return response;
+}
+
+function countDown(){
+    let y=10
+    while(y>=0){
+        console.log(y--);
+
+    }
+}
